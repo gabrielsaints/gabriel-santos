d80747daf060d87fe07f25dbaa5426f8f4612147
Gabriel Santos <gabr1el_santos@hotmail.com>
2019-04-14 21:05:11 +0000

    Add vscode design structure to the site

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
+import { MdCode, MdComputer } from "react-icons/md";
+
 import "./styles.css";
 
 class Main extends Component {
   render() {
     return (
       <div id="main-container">
-        <form action="">
-          <button type="submit">Criar</button>
-        </form>
+        <div className="header">
+          <ul className="header-menu">
+            <li className="header-menu-item">
+              <MdCode className="header-menu-icon" />
+            </li>
+            <li className="header-menu-item">File</li>
+            <li className="header-menu-item">Edit</li>
+            <li className="header-menu-item">{this.state.sidemenu}</li>
+          </ul>
+        </div>
+        <div className="code-page">
+          <div className="side-left-menu">
+            <div className="extensions-menu">
+              <span className="extensions-font">G</span>
+              <span className="extensions-font">A</span>
+              <span className="extensions-font">B</span>
+              <span className="extensions-font">R</span>
+              <span className="extensions-font">I</span>
+              <span className="extensions-font">E</span>
+              <span className="extensions-font">L</span>
+            </div>
+
+            <div className="files-menu" />
+          </div>
+          <div className="main-page">
+            <div className="links-open" />
+            <div className="tree" />
+            <div className="code-area" />
+          </div>
+        </div>
       </div>
     );
   }
